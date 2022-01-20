e24f6f22e4e3d4fbf94e8a4eb7d031b41619bc31
viforget <victor.forget99@gmail.com>
2022-01-20 15:46:07 +0100

    Add iterators function, random_iterators and reverse_random_iterator

--- srcs/iterator.hpp
+++ srcs/iterator.hpp
@@ -1,21 +1,139 @@
-#ifndef VECTOR_HPP
-# define VECTOR_HPP
+#ifndef ITERATOR_HPP
+# define ITERATOR_HPP
 
 # include <iostream>
 # include <memory>
 
 namespace	ft
 {
-	template<class Category, class T, class Distance = std::ptrdiff_t, class Pointer = T*, class Reference = T&>
+	template</*class Category, */class T, class Distance = std::ptrdiff_t, class Pointer = T*, class Reference = T&>
 	struct iterator
 	{
-		Category	iterator_category
-		T			value_type;
-		Distance	difference_type;
-		Pointer		pointer;
-		Reference	reference;
+		//typedef Category	iterator_category;
+		typedef T			value_type;
+		typedef Distance	difference_type;
+		typedef Pointer		pointer;
+		typedef Reference	reference;
+	};
+	
+	template < class T >
+	class random_iterator
+	{
+		public:
+			//typedef ft::iterator<T>::iterator_category	iterator_category;
+			typedef typename ft::iterator<T>::value_type		value_type;
+			typedef typename ft::iterator<T>::difference_type	difference_type;
+			typedef typename ft::iterator<T>::pointer			pointer;
+			typedef typename ft::iterator<T>::reference			reference;
+			
+//---------- Constructors ----------//
+
+			random_iterator( void ) : _pointer(NULL) {}
+			random_iterator( pointer pointer ) : _pointer(pointer) {}
+
+//---------- Destructor ----------//
+
+			~random_iterator( void ) {};
+			
+//---------- Operators ----------//
+
+			void operator=(random_iterator const & ref)
+			{
+				this->_pointer = ref._pointer;
+			}
+			
+			reference operator*() const
+			{
+				return(*this->_pointer);
+			}
+			
+			random_iterator&	operator++()
+			{
+				this->_pointer++;
+				return (*this);
+			}
+			random_iterator		operator++(int)
+			{
+				random_iterator tmp = *this;
+				this->_pointer++;
+				return (tmp);
+			}
+
+			random_iterator&	operator--()
+			{
+				this->_pointer--;
+				return (*this);
+			}
+			random_iterator		operator--(int)
+			{
+				random_iterator tmp = *this;
+				this->_pointer--;
+				return (tmp);
+			}
+
+		private:
+			pointer	_pointer;
 	};
 
+	template < class T >
+	class reverse_random_iterator
+	{
+		public:
+			//typedef ft::iterator<T>::iterator_category	iterator_category;
+			typedef typename ft::iterator<T>::value_type		value_type;
+			typedef typename ft::iterator<T>::difference_type	difference_type;
+			typedef typename ft::iterator<T>::pointer			pointer;
+			typedef typename ft::iterator<T>::reference			reference;
+			
+//---------- Constructors ----------//
+
+			reverse_random_iterator( void ) : _pointer(NULL) {}
+			reverse_random_iterator( pointer pointer ) : _pointer(pointer) {}
+
+//---------- Destructor ----------//
+
+			~reverse_random_iterator( void ) {};
+			
+//---------- Operators ----------//
+
+			void operator=( reverse_random_iterator const & ref )
+			{
+				this->_pointer = ref._pointer;
+				return (this);
+			}
+			
+			reference operator*() const
+			{
+				return(*this->_array);
+			}
+			
+			reverse_random_iterator&	operator++()
+			{
+				this->_pointer--;
+				return (*this);
+			}
+			reverse_random_iterator		operator++(int)
+			{
+				reverse_random_iterator tmp = *this;
+				this->_pointer--;
+				return (tmp);
+			}
+
+			reverse_random_iterator&	operator--()
+			{
+				this->_pointer++;
+				return (*this);
+			}
+			reverse_random_iterator		operator--(int)
+			{
+				reverse_random_iterator tmp = *this;
+				this->_pointer++;
+				return (tmp);
+			}
+
+		private:
+			pointer	_pointer;
+	};
 };
 
 #endif
--- srcs/vector.hpp
+++ srcs/vector.hpp
@@ -4,6 +4,7 @@
 # include <iostream>
 # include <memory>
 # include <iterator>
+# include <stdexcept>
 # include "iterator.hpp"
 
 namespace	ft
@@ -17,24 +18,32 @@ namespace	ft
 		std::size_t	_capacity;
 		Alloc		_alloc;	
 	public:
-		typedef T											value_type;
-		typedef Alloc										allocator_type;
-		typedef typename allocator_type::reference			reference;
-		typedef typename allocator_type::const_reference	const_reference;
-		typedef typename Alloc::pointer						pointer;
-		typedef typename Alloc::const_pointer				const_pointer;
-		typedef	std::random_access_iterator_tag				iterator;
-		//typedef	std::random_access_iterator_tag<const value_type>	const_iterator;
-		typedef std::reverse_iterator<iterator>				reverse_iterator;
-		//typedef std::reverse_iterator<const_iterator>		const_reverse_iterator;
-		typedef std::ptrdiff_t								difference_type;
-		typedef std::size_t									size_type;
+		typedef T												value_type;
+		typedef Alloc											allocator_type;
+		typedef typename allocator_type::reference				reference;
+		typedef typename allocator_type::const_reference		const_reference;
+		typedef typename Alloc::pointer							pointer;
+		typedef typename Alloc::const_pointer					const_pointer;
+		typedef	ft::random_iterator<value_type>					iterator;
+		typedef	ft::random_iterator<const value_type>			const_iterator;
+		typedef ft::reverse_random_iterator<iterator>			reverse_iterator;
+		typedef ft::reverse_random_iterator<const_iterator>	const_reverse_iterator;
+		typedef std::ptrdiff_t									difference_type;
+		typedef std::size_t										size_type;
 
+
+
+//---------- Constructors ----------//
+
+		//First constructor; don't take annything exepct a non-mandatory allocator
+		//Create an empty array
 		explicit vector (const allocator_type& alloc = allocator_type()) : _size(0), _capacity(0), _alloc(alloc)
 		{
 			this->_array = this->_alloc.allocate(0);
 		}
 
+		//Second constructor; take a size (n) and a value (val)
+		//Create an array of size n, every data in it is val
 		explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) : _size(n), _capacity(n), _alloc(alloc)
 		{
 			this->_array = this->_alloc.allocate(n);
@@ -42,10 +51,16 @@ namespace	ft
 				this->_alloc.construct(this->_array + i, val);
 		}
 
+		//Third constructor; take 2 Iterator (first)(last)
+		//Create an array with every data between first and last
+		//If the first iterator is further than the last one; throw an exception
 		template <class InputIterator>
         vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()) : _size(0), _capacity(0), _alloc(alloc)
 		{
 			InputIterator first2 = first;
+			
+			if (first > last)
+				throw(std::length_error("vector"));
 			for (; first2 < last; first2++)
 				this->_size++;
 			this->_array = this->_alloc.allocate(this->_size++);
@@ -54,6 +69,8 @@ namespace	ft
 				this->_alloc.construct(this->_array + i, *(first + i));
 		}
 
+		//4th constructor; take an other vector
+		//Create a deep copy of the first vector
 		vector (const vector& x) : _size(x._size), _capacity(x._capacity), _alloc(x._alloc)
 		{
 			this->_array = this->_alloc.allocate(this->_capacity);
@@ -61,18 +78,24 @@ namespace	ft
 				this->_alloc.construct(this->_array + i, x._array[i]);
 		}
 
+//---------- Destructor ----------//
+
+		//Destructor : Destroy every data and deallocate the array
 		~vector ()
 		{
-			for (std::size_t i; i < this->_size; i++)
-				this->_alloc.deconstruct(this->_array + i);
-			this->_alloc.deallocate(this->_array);
+			for (std::size_t i = 0; i < this->_size; i++)
+				this->_alloc.destroy(this->_array + i);
+			this->_alloc.deallocate(this->_array, this->_capacity);
 		}
 
+//---------- Operator= ----------//
+
+		// operator = : Destroy every data and deallocate the array then make a deep copy of the other vector
 		vector& operator= (const vector& x)
 		{
-			for (for (std::size_t i = 0; i < this->_size; i++)
-				this->_alloc.deconstruct(this->_array + i);
-			this->_alloc.deallocate(this->_array);
+			for (std::size_t i = 0; i < this->_size; i++)
+				this->_alloc.destroy(this->_array + i);
+			this->_alloc.deallocate(this->_array, this->_capacity);
 			this->_size = x._size;
 			this->_capacity = x._capacity;
 			this->_alloc = x._alloc;
@@ -80,8 +103,49 @@ namespace	ft
 			for (std::size_t i = 0; i < this->_size; i++)
 				this->_alloc.construct(this->_array + i, x._array[i]);
 		}
-	};
 
+//---------- Iterators ----------//
+		
+		iterator begin()
+		{
+			return (iterator(this->_array));
+		}
+
+		const_iterator begin() const
+		{
+			return (const_iterator(this->_array));
+		}
+
+		iterator end()
+		{
+			return (iterator(this->_array + this->_size));
+		}
+
+		const_iterator end() const
+		{
+			return (const_iterator(this->_array + this->_size));
+		}
+
+		iterator rbegin()
+		{
+			return (iterator(this->_array + this->_size));
+		}
+
+		const_iterator rbegin() const
+		{
+			return (const_iterator(this->_array + this->_size));
+		}
+
+		iterator rend()
+		{
+			return (iterator(this->_array));
+		}
+
+		const_iterator rend() const
+		{
+			return (const_iterator(this->_array));
+		}
+	};
 };
 
 #endif
