7325e1e3fddc2c714665d600dbe8134cee0e5562
Victor Forget <viforget@pop-os.localdomain>
2022-02-20 21:17:50 +0100

    create map iterator

---
+++ srcs/map_iterator.hpp
@@ -0,0 +1,56 @@
+#ifndef MAP_ITERATOR_HPP
+# define MAP_ITERATOR_HPP
+
+# include "map_utils.hpp"
+# include "binary_tree.hpp"
+
+namespace	ft
+{
+	template <class T>
+	class map_iterator
+	{
+	private:
+		_node	*n;
+	public:
+//---------- Constructors ----------//
+
+		map_iterator() : _node(NULL) ();
+		map_iterator( const map_iterator & ref) : _node(ref.node) ();
+		map_iterator( const node & ref) : _node(node) ();
+
+//---------- Destructor ----------//
+
+		~map_iterator() {}
+
+//---------- Operators ----------//
+
+		void	operator=(const map_iterator & ref)
+		{
+			this->_node = ref.node;
+		}
+	
+		T*		operator*() const
+		{
+			return (this->node.data);
+		}
+
+		map_iterator &	operator++()
+		{
+			node	*n;
+
+			this->_node;
+			if (!this->node->left->leaf())
+			{
+				n = this->node->left;
+				while(!n->left->leaf())
+					n = n->left;
+				this->_node = n;
+				return(*this);
+			}
+			
+		}
+	};
+	
+}
+
+#endif
